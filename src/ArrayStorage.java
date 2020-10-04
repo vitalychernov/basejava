70167ebe15df641a433c1e1a6f1cec5fbb8f8f3d
vitalychernov <chernovitaly@gmail.com>
2020-10-03 21:50:51 -0400

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,59 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                System.out.println("Resume " + r.uuid + " already exists");
+                return;
+            }
+        }
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        } else {
+            System.out.println("Storage is full");
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i] != null && uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            storage[i] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            allResumes[i] = storage[i];
+        }
+        return allResumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
