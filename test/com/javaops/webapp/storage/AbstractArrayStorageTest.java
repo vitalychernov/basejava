7d9793872aa93e96360c080b8f3188d227bde94a
vitalychernov <chernovitaly@gmail.com>
2020-10-28 08:47:13 -0400

    Update HW4

--- test/com/javaops/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/javaops/webapp/storage/AbstractArrayStorageTest.java
@@ -36,9 +36,14 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void get() {
-        Assert.assertEquals(RESUME_1, storage.get(RESUME_1.getUuid()));
-        Assert.assertEquals(RESUME_2, storage.get(RESUME_2.getUuid()));
-        Assert.assertEquals(RESUME_3, storage.get(RESUME_3.getUuid()));
+        Assert.assertEquals(RESUME_1, storage.get(UUID_1));
+        Assert.assertEquals(RESUME_2, storage.get(UUID_2));
+        Assert.assertEquals(RESUME_3, storage.get(UUID_3));
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() throws Exception {
+        storage.get("dummy");
     }
 
     @Test
@@ -69,7 +74,7 @@ public abstract class AbstractArrayStorageTest {
     public void save() {
         Assert.assertEquals(3, storage.size());
         storage.save(new Resume(UUID_4));
-        Assert.assertEquals(RESUME_4, storage.get(RESUME_4.getUuid()));
+        Assert.assertEquals(RESUME_4, storage.get(UUID_4));
         Assert.assertEquals(4, storage.size());
     }
 
@@ -78,16 +83,17 @@ public abstract class AbstractArrayStorageTest {
         storage.save(RESUME_1);
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() {
         Assert.assertEquals(3, storage.size());
         storage.delete(UUID_3);
         Assert.assertEquals(2, storage.size());
+        storage.get(UUID_3);
     }
 
     @Test(expected = NotExistStorageException.class)
     public void deleteNotExist() {
-        storage.delete(RESUME_4.getUuid());
+        storage.delete(UUID_4);
     }
 
     @Test
@@ -97,11 +103,6 @@ public abstract class AbstractArrayStorageTest {
         Assert.assertEquals(RESUME_1, resumes[0]);
         Assert.assertEquals(RESUME_2, resumes[1]);
         Assert.assertEquals(RESUME_3, resumes[2]);
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void getNotExist() throws Exception {
-        storage.get("dummy");
     }
 
     @Test(expected = StorageException.class)
