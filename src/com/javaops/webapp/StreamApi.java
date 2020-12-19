9f5c566cea990349ed08f655e7059b1f1b4ca5d9
vitalychernov <chernovitaly@gmail.com>
2020-12-18 21:07:14 -0500

    Update HW12
    StreamApi
    Сдвинул все приватные методы в конец класса
    Все операции написал с новой строки в стримах
    
    minValue()
    Убрал getAsInt()
    
    oddOrEven()
    Перенес проверку на четность sum в стрим

--- src/com/javaops/webapp/StreamApi.java
+++ src/com/javaops/webapp/StreamApi.java
@@ -6,23 +6,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamApi {
-    private static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce((x, y) -> x * 10 + y).getAsInt();
-    }
-
-    private static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = integers.stream().mapToInt(Integer::intValue).sum();
-        System.out.println(sum);
-        if (sum % 2 == 0) {
-            return integers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
-        } else
-            return integers.stream().filter(i -> i % 2 != 0).collect(Collectors.toList());
-    }
 
     public static void main(String[] args) {
         int[] values = {1, 1, 9, 3, 5, 4, 4, 1, 6};
         System.out.println(minValue(values));
-        List<Integer> list = new ArrayList<>(Arrays.asList(1, 1, 9, 3, 5, 4, 4, 1, 6));
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 9, 3, 5, 4, 4, 1, 6));
         System.out.println(oddOrEven(list));
+    }
+
+    private static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (x, y) -> x * 10 + y);
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sum);
+        return integers.stream()
+                .filter(sum % 2 == 0 ? (i -> i % 2 == 0) : i -> i % 2 != 0)
+                .collect(Collectors.toList());
     }
 }
