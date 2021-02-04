65f83f7712652c91daf21885be9b80bd398428fb
Vitaly Chernov <61357201+vitalychernov@users.noreply.github.com>
2021-02-03 22:36:13 -0500

    Delete HTMLUtil.java

--- src/com/javaops/webapp/util/HTMLUtil.java
+++
@@ -1,9 +0,0 @@
-package com.javaops.webapp.util;
-
-import com.javaops.webapp.model.Organization;
-
-public class HTMLUtil {
-    public static String formatDates(Organization.Position position) {
-        return DateUtil.format(position.getStartDate()) + " - " + DateUtil.format(position.getEndDate());
-    }
-}
