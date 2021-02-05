33eb3ecd11efd9d9319563630272b23875b55b34
vitalychernov <chernovitaly@gmail.com>
2021-02-05 15:28:14 -0500

    HtmlUtil update

--- src/com/javaops/webapp/util/HtmlUtil.java
+++ src/com/javaops/webapp/util/HtmlUtil.java
@@ -1,9 +1,32 @@
 package com.javaops.webapp.util;
 
-import com.javaops.webapp.model.Organization;
+import com.javaops.webapp.model.*;
 
-public class HTMLUtil {
+import java.util.List;
+
+public class HtmlUtil {
+    public static boolean isEmpty(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
     public static String formatDates(Organization.Position position) {
         return DateUtil.format(position.getStartDate()) + " - " + DateUtil.format(position.getEndDate());
+    }
+
+    public static String getText(SectionType type, Resume resume) {
+        AbstractSection section = resume.getSection(type);
+        return section == null ? "" : ((TextSection) resume.getSection(type)).getText();
+    }
+
+    public static String getItems(SectionType type, Resume resume) {
+        AbstractSection section = resume.getSection(type);
+        return
+                section == null ? "" :
+                        String.join("\n", ((ListSection) resume.getSection(type)).getItems());
+    }
+
+    public static List<Organization> getOrganizations(SectionType type, Resume resume) {
+        AbstractSection section = resume.getSection(type);
+        return section == null ? null : ((OrganizationSection) resume.getSection(type)).getOrganizations();
     }
 }
